d92271cc7886f9a9b8d76f325c2e2fc31b23eba7
Wei Qi Teo <weiqi.teo.2025@computing.smu.edu.sg>
2026-09-14 09:13:39 +0800

    7

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -5,26 +5,24 @@ const teamA = ref("Falcons")
 const teamB = ref("Tigers")
 const scoreA = ref(0)
 const scoreB = ref(0)
-const step = ref(1) // points added per click
+const step = ref(1)
 const maxScore = ref(10)
 
 function addA() {
-    scoreA.value = Math.min(maxScore.value, scoreA.value + step.value);
+    scoreA.value = Math.min(maxScore.value, scoreA.value + step.value)
 }
 
 function addB() {
-    scoreB.value = Math.min(maxScore.value, scoreB.value + step.value);
+    scoreB.value = Math.min(maxScore.value, scoreB.value + step.value)
 }
 
 function reset() {
-    scoreA.value = 0;
-    scoreB.value = 0;
+    scoreA.value = 0
+    scoreB.value = 0
 }
-
 </script>
 
 <template>
-
     <div style="font-family: Arial; max-width: 520px; margin: 24px auto;">
         <h2>Mini Scoreboard</h2>
 
@@ -33,13 +31,12 @@ function reset() {
         <p>Current: {{ scoreA }} - {{ scoreB }}</p>
 
         <!-- B. In-template expressions go here -->
-         <p>Total points: {{ scoreA + scoreB }}</p>
+        <p>Total points: {{ scoreA + scoreB }}</p>
 
-         <p>Points left to win: 
-            <!-- {{ maxScore - (scoreA > scoreB ? scoreA : scoreB) }} -->
+        <p>
+            Points left to win:
             {{ maxScore - Math.max(scoreA, scoreB) }}
         </p>
-
 
         <!-- A. Event handlers go here -->
         <div style="display: flex; gap: 12px; margin: 12px 0;">
@@ -48,17 +45,21 @@ function reset() {
             <button @click="reset">Reset</button>
         </div>
 
-
         <div style="margin-top: 14px;">
             <!-- C. Display winner / status here -->
-            <p v-if="scoreA == maxScore">Winner: {{ teamA }}</p>
-            <p v-else-if="scoreB == maxScore">Winner: {{ teamB }}</p>
-            <p v-else>No winner yet. Keep playing</p>
+            <p v-if="scoreA >= maxScore">
+                Winner: {{ teamA.toUpperCase() }}
+            </p>
+
+            <p v-else-if="scoreB >= maxScore">
+                Winner: {{ teamB.toUpperCase() }}
+            </p>
+
+            <p v-else>
+                No winner yet. Keep playing
+            </p>
         </div>
-
-
     </div>
-
 </template>
 
 <style scoped>
